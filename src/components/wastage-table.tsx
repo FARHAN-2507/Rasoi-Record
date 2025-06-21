1d52e6a75322b73592611196c1efc602d5a5d626
Farhan <farhanakthar2507@gmail.com>
2025-06-21 09:02:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/wastage-table.tsx
+++ src/components/wastage-table.tsx
@@ -16,7 +16,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import {
